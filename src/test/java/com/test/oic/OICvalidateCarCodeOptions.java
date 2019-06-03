1b00ab24f1348e6318453d46c25955a5d33a5779
pisitphaungsak <pisit.phaungsak@gmail.com>
2019-06-03 14:08:23 +0700

    Added OICvalidateCarCodeOptions test class

---
+++ src/test/java/com/test/oic/OICvalidateCarCodeOptions.java
@@ -0,0 +1,18 @@
+package com.test.oic;
+
+import org.testng.annotations.Test;
+
+public class OICvalidateCarCodeOptions extends BaseOIC{
+
+    @Test
+    public void validateValidCarCodeOptions(){
+        //Assert
+
+        //Act
+
+        //Assert
+
+
+
+    }
+}
