db6f2adb6edbe186f80b18eded2fcc56cacee4c7
pisitphaungsak <pisit.phaungsak@gmail.com>
2019-06-14 21:20:04 +0700

    End of day 14 June 2019

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,5 +1,11 @@
 package runner;
 
 
-public class TestRunner {
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+
+
+@CucumberOptions(features = {"src/test/java/features/"}, glue = "steps")
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
