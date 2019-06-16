ff6f7a9dc525eb97a720071193fe91750bc050e8
pisitphaungsak <pisit.phaungsak@gmail.com>
2019-06-16 21:44:58 +0700

    Add database conn

--- src/test/java/GetPostSteps.java
+++
@@ -1,7 +0,0 @@
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import utilities.RestAssuredExtenstion;
-
-public class GetPostSteps {
-
-}
---
+++ src/test/java/utilities/DBUtils.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+    private final String url = "jdbc:postgresql://sunday-ci-rds.cmbagwipzfnw.ap-southeast-1.rds.amazonaws.com:5432/staging-01";
+    private final String user = "sunday_ci";
+    private final String password = "sunday_ci123#";
+
+    private Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+
+}
